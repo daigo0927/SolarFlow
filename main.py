50b0dcc6f3dfae560ce29173865cdf8f1e93dedd
Daigo HIROOKA <tappy.hello-planet@ezweb.ne.jp>
2017-05-20 17:39:27 +0900

    add main script

---
+++ main.py
@@ -0,0 +1,57 @@
+# coding:utf-8
+
+import os, sys
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pickle
+
+from tqdm import tqdm
+
+from SolarFlow import SolarFlow
+from misc.visualize import draw_cloud
+
+
+args = ['main.py', './somesolar.pkl', '-1', '5', '2']
+args[:len(sys.argv)] = sys.argv
+
+def main():
+
+    _, datapath, limit_frame, Srange, Nrange = args
+    limit_frame = int(limit_frame)
+    Srange = int(Srange)
+    Nrange = int(Nrange)
+
+    with open(datapath, 'rb') as f:
+        data = pickle.load(f)
+
+    if limit_frame > 0:
+        print('interp only data[:{}]'.format(limit_frame))
+        data = data[:int(limit_frame)]
+
+    sflow = SolarFlow(data = data,
+                      SearchRange = Srange,
+                      NeighborRange = Nrange)
+
+    method = input('select interpolation method [for/bi] : ')
+    fine = int(input('interpolation fineness : '))
+    sflow.interp(fineness = fine, method = method)
+    
+    save = input('save interp-result image? [y/n] : ')
+    if save == 'y':
+        if not os.path.exists('./image'):
+            os.mkdir('./image')
+        for i in tqdm(range(sflow.result.shape[0])):
+            draw_cloud(sflow.data[i//fine], sflow.result[i])
+            plt.savefig('./image/{}{}.png'.format(method, i))
+            plt.close()
+    elif save == 'n':
+        print('not save interp-result')
+        pass
+
+    print('successfully finished')
+    
+    return sflow.result
+            
+if __name__ == '__main__':
+    main()
