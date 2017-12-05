25688ed52f5038d25884c01deab98fd4b408ee68
Daigo HIROOKA <tappy.hello-planet@ezweb.ne.jp>
2017-12-06 03:12:23 +0900

    initial commit for global solar radiation error

--- optimize.py
+++ optimize.py
@@ -1,17 +1,17 @@
 
 import numpy as np
 from hyperopt import hp, tpe, Trials, fmin
-from SmoothFlow_Vreg import VectorOptimizer
+from SmoothFlow_Vreg import smoothflow_withVreg
+from SmoothInterp import fullinterp
 
-
-
-def opt_hyper(fullframes,
-              n_range = 2, s_range = 5,
+def opt_hyper(data,
+              n_range = 2, s_range = 7,
               losstype = 'MSE',
               max_evals = 100,
               st_reg = True):
 
-    opt_vec = VectorOptimizer(fullframes, n_range, s_range, losstype)
+    frames_train_for = data['shade'][::2]
+    frames_train_back = data['shade'][::-2]
 
     hyper_params = {}
     hyper_params['k1'] = hp.uniform('spatial', 0, 0.1)
@@ -19,14 +19,21 @@ def opt_hyper(fullframes,
     if st_reg:
         hyper_params['k3'] = hp.uniform('spatiotemporal', 0, 0.1)
 
-    def _coefmake(k1 = 0, k2 = 0, k3 = 0):
-        return np.array([1, k1, k2, k3])
-
     # objective function
-    def _objective(args):
-        coef = _coefmake(**args)
-        opt_vec._vec_init()
-        _, loss = opt_vec.optimize(num_iter = 5, coef = coef)
+    def solar_reconst(args):
+        coef = _makecoef(**args)
+        vec_for = smoothflow_withVreg(fullframes = frames_train_for,
+                                      coef = coef,
+                                      neighbor_range = n_range,
+                                      search_range = s_range,
+                                      losstype = losstype)
+        vec_back = smoothflow_withVreg(fullframes = frames_train_back,
+                                       coef = coef,
+                                       neighbor_range = n_range,
+                                       search_range = s_range,
+                                       losstype = losstype)
+        shade_reconst = fullinterp(frames_train_for, vec_for, vec_back, 2)
+        total_reconst = aaa # サイズ合わせ
         return np.sum(loss)
 
     trials = Trials()
@@ -40,4 +47,8 @@ def opt_hyper(fullframes,
     )
                 
     return best, trials
-        
+
+def _makecoef(k1 = 0, k2 = 0, k3 = 0):
+    return np.array([1, k1, k2, k3])
+
+
