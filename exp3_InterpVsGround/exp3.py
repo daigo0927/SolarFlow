277ccd926a458c4a573d9339988f483357b22f8b
Daigo HIROOKA <tappy.hello-planet@ezweb.ne.jp>
2017-12-20 18:16:58 +0900

    confirm exp3 working

--- exp3_InterpVsGround/exp3.py
+++ exp3_InterpVsGround/exp3.py
@@ -9,6 +9,7 @@ import argparse
 import pdb
 
 from tqdm import tqdm
+from fabric.colors import green
 from collections import OrderedDict
 
 from misc.visualize import draw_cloud
@@ -31,8 +32,8 @@ def _process(pkldir, gdata_dir, date, region_name, limit_frame, max_evals):
     limit_frame = min(limit_frame, data['crop'].shape[0])
     tdata = data['crop'][:limit_frame]
     sdata = data['shade'][:limit_frame]
-    f_origin, w_origin, _ = tdata.shape
-    obj_idx = [19, 20] # ground measurement location
+    _, frame_size_origin, _ = tdata.shape
+    obj_idx = np.array([19, 20]) # ground measurement location for the 120th building
     # total radiation at objective location, shppe(217, )
     obj_total = tdata[:limit_frame, obj_idx[1], obj_idx[0]]
     # outer radiation at objective location, shape(3241, )
@@ -41,7 +42,7 @@ def _process(pkldir, gdata_dir, date, region_name, limit_frame, max_evals):
 
     # load ground data
     gpath = gdata_dir + '/' + ''.join(date.split('-')) + '_1sec.csv'
-    processor = gdata_preprocesser(gdata_dir)
+    processor = gdata_preprocesser(gpath)
     gdata = processor.move_avg(limit_frame, 150)
     gdata_fine = processor.move_avg(limit_frame, 10)
 
@@ -50,73 +51,55 @@ def _process(pkldir, gdata_dir, date, region_name, limit_frame, max_evals):
     n_range = int(2)
     fineness = int(15)
 
-    shades = OrderedDict()
+    result = OrderedDict()
+    inter = Interpolater(data = data, limit_frame = limit_frame, fineness = 15)
+    result['linear'] = inter.linear_interp()
+    result['normal_MSE'] = inter.flow_interp(losstype = 'MSE')
+    result['normal_NCC'] = inter.flow_interp(losstype = 'NCC')
+    result['double_MSE'], hparams_d_MSE = inter.flow_interp_doubleregs(losstype = 'MSE',
+                                                                       max_evals = max_evals)
+    result['double_NCC'], hparams_d_NCC = inter.flow_interp_doubleregs(losstype = 'NCC',
+                                                                       max_evals = max_evals)
+    result['triple_MSE'], hparams_t_MSE = inter.flow_interp_tripleregs(losstype = 'MSE',
+                                                                       max_evals = max_evals)
+    result['triple_NCC'], hparams_t_NCC = inter.flow_interp_tripleregs(losstype = 'NCC',
+                                                                       max_evals = max_evals)
 
-    # linear interp : shape(3241, 30, 30)
-    print('linear interpolating')
-    lin = LinearInterp(data = sdata, crop = s_range)
-    lin.interp(fineness = fine)
-    shades['linear'] = lin.result
-
-    # biflow interp : shape(3241, 30, 30)
-    print('biflow interpolating')
-    easyflow = easySolarFlow(data = sdata,
-                             search_range = s_range,
-                             neighbor_range = n_range)
-    easyflow.interp(fineness = fine, method = 'bi')
-    shades['bi'] = easyflow.result
-
-    # proposed method 1, temporal smoothing : shape(217, 23?, 23?)
-    print('temporal smoothed interpolating ...')
-    t_smooth = SmoothInterp(data = sdata,
-                            search_range = s_range,
-                            neighbor_range = n_range)
-    t_smooth.interp(frameset_size = 3, # <- temporal smoothing
-                    space_smooth_value = 10, fineness = fine)
-    shades['temp_smooth'] = t_smooth.result
-    
-    # proposed method 2, spatial smoothing, and double smoothing : shape(217, 23?, 23?)
-    s_smooth_values = np.array([6., 3., 1.5])
-    labels = ['weak', 'middle', 'strong']
-    for s_value, label in zip(s_smooth_values, labels):
-        s_smooth = SmoothInterp(data = sdata,
-                                search_range = s_range,
-                                neighbor_range = n_range)
-        print('{} spatial smoothed interpolating ...'.format(label))
-        s_smooth.interp(frameset_size = 2,
-                        space_smooth_value = s_value, fineness = fine)
-        shades['space_smooth_{}'.format(label)] = s_smooth.result
-        
-        print('{} double smoothed interpolating ...'.format(label))
-        s_smooth.interp(frameset_size = 3,
-                        space_smooth_value = s_value, fineness = fine)
-        shades['double_smooth_{}'.format(label)] = s_smooth.result
-
-
-    # confirm interpolation results
-    error = np.zeros((f_fine, len(shades.keys())+1))
-    error[:,:] = None
+    # record error
+    simular = np.zeros((limit_frame - 1, len(result.keys())))
     colnames = []
-    # original 2.5min total radiation comparing
-    error[::15,0] = ((obj_total - gdata)**2)**(1/2)
-    colnames.append('origin')
-    for i, key in enumerate(shades.keys()):
-        shade = shades[key]
-        f, w, _ = shade.shape
-        drop = int((w_origin - w)/2)
-        obj_shade = shade[:, obj_idx[1]-drop, obj_idx[0]-drop]
-        obj_total_fine = obj_outer_fine[:f]*(1. - obj_shade)
-        error[:len(obj_total_fine), i+1] =\
-                            ((obj_total_fine - gdata_fine[:f])**2)**(1/2)
+    print(green('interpolating results testing by ground measured data'))
+    for i, (key, res) in enumerate(result.items()):
+        tvalue = pickPointValue(tvalues = res,
+                                target_idx_origin = obj_idx,
+                                frame_size_origin = frame_size_origin)
+        simular[:, i] = np.array([cosSimular(tvalue[i*fineness:(i+1)*fineness],
+                                             gdata_fine[i*fineness:(i+1)*fineness])
+                                  for i in np.arange(limit_frame - 1)])
         colnames.append(key)
-        
-    error = pd.DataFrame(error)
-    error.columns = colnames
-    print(error.describe())
-        
-    return [region_name+date, error]
-    
 
+    simular = pd.DataFrame(simular)
+    simular.columns = colnames
+    print(simular.describe())
+        
+    return {'date':region_name+date, 'simularity':simular,
+            'hparams_d_MSE':hparams_d_MSE, 'hparams_d_NCC':hparams_d_NCC,
+            'hparams_t_MSE':hparams_t_MSE, 'hparams_t_NCC':hparams_t_NCC}
+
+# extract target point radiation value, basically from total radiation value
+# this func prepared for that interpolated result can be different size from original data
+def pickPointValue(tvalues, target_idx_origin, frame_size_origin):
+    _, frame_size, _ = tvalues.shape
+    rem = int((frame_size_origin - frame_size)/2)
+    target_idx = target_idx_origin - rem
+    return tvalues[:, target_idx[1], target_idx[0]]
+
+def cosSimular(vec1, vec2):
+    norm1 = np.sqrt(np.sum(vec1**2))
+    norm2 = np.sqrt(np.sum(vec2**2))
+    cossim = np.sum(vec1*vec2)/norm1/norm2
+    return cossim
+    
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--sdata_dir', type = str, required = True,
@@ -129,19 +112,25 @@ def main():
                         help = 'region name, under /row_data/yyyy-mm-dd/pickles, like Tokyo')
     parser.add_argument('--limit_frame', type = int, default = 999,
                         help = 'number of frames for utilize, default [999](all)')
+    parser.add_argument('--max_evals', type = int, default = 10,
+                        help = 'max evaluation in hyper-parameter optimize [10]')
     args = parser.parse_args()
 
     pkldirs = [args.sdata_dir + '/' + d + '/pickles/' + args.region_name\
                for d in args.date]
     attrs = [(pkldir, args.gdata_dir, d, args.region_name,
-              args.limit_frame) for pkldir, d in zip(pkldirs, args.date)]
+              args.limit_frame, args.max_evals)
+             for pkldir, d in zip(pkldirs, args.date)]
 
     result = OrderedDict()
     num_cores = int(input('input utilize core number : '))
-    pool = Pool(num_cores)
-    result['error'] = list(pool.map(_wrapper, attrs))
-    pool.close()
-    
+    if num_cores <= 1:
+        result['result'] = [_wrapper(attr) for attr in attrs]
+    else:
+        pool = Pool(num_cores)
+        result['result'] = list(pool.map(_wrapper, attrs))
+        pool.close()
+        
     result['config'] = args
 
     with open('./result.pkl', 'wb') as f:
@@ -149,4 +138,3 @@ def main():
             
 if __name__ == '__main__':
     main()
-    
